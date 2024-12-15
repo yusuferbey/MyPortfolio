167ed464d3ecf0e4b216099205d9beab5ff8a6bd
Yusuf Erbey <erbeyy187@gmail.com>
2024-12-15 16:46:42 +0300

    güncelleme

---
+++ ViewComponents/_NavbarComponentsPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyPortfolio.ViewComponents
+{
+    public class _NavbarComponentsPartial:ViewComponent
+    {
+        public IViewComponentResult Invoke() 
+        {
+            return View();
+        }
+    }
+}
